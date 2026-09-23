0ca51b982d421456dbd6a08d6a18142f1094b111
Arun Yogi <yogiarun290@gmail.com>
2026-09-23 11:38:49 +0545

    feat : intro to js

---
+++ javascript/script.js
@@ -0,0 +1,36 @@
+// Single Line Comment: This is a single line comment in JavaScript
+
+console.log("Hello, World!");
+// console.log("Ram Thapa");
+console.log('Broadway');
+/*console.log(99);
+console.log(3.15); This is a multi-line comment in JavaScript
+console.log(true);*/
+console.log(false);
+
+
+// variable => name location in storage whose value can be changed
+let username = "Ram Thapa";
+console.log(username);
+console.log("username");
+username = 'Sita Thapa';
+console.log(username);
+
+console.log('Hello ' + username);
+
+//Basic arithmetic operations
+let num1 = 10;
+let num2 = 5;
+let sum = num1 + num2;
+console.log("Sum of " + num1 + " and " + num2 + " is: " + sum);
+let sub = num1 - num2;
+console.log("Subtraction of " + num1 + " and " + num2 + " is: " + sub);
+
+let mul = num1 * num2;
+console.log("Multiplication of " + num1 + " and " + num2 + " is: " + mul);
+
+let div = num1 / num2;
+console.log("Division of " + num1 + " and " + num2 + " is: " + div);
+
+let mod = num1 % num2;
+console.log("Modulus of " + num1 + " and " + num2 + " is: " + mod);
