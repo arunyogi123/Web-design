6722c13bacd119a17989504d6599e9b4f124e644
Arun Yogi <yogiarun290@gmail.com>
2026-09-24 10:56:49 +0545

    feat : practice jquery

---
+++ jquery/faq.js
@@ -0,0 +1,3 @@
+$(".faq-question").click(function () {
+    $(this).next(".faq-answer").slideToggle();
+});
---
+++ jquery/script.js
@@ -0,0 +1,31 @@
+
+
+// $("#add").click(function(){
+//     $("#box").html("hello world");
+// });
+
+
+// $("#hidebox").click(function(){
+//     $("#box").hide();
+// })
+// $("#show").click(function(){
+//     $("#box").show();
+// })
+// $("#fadeout").click(function(){
+//     $("#box").fadeOut();
+// })
+// $("#fadein").click(function(){
+//     $("#box").fadeIn();
+// })
+// $("#slideup").click(function(){
+//     $("#box").slideUp();
+// })
+// $("#slidedown").click(function(){
+//     $("#box").slideDown();
+// })
+
+
+$("nav a").click(function(){
+    $("nav a").removeClass("active");
+    $(this).addClass("active");
+})
