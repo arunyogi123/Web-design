d9d809a5e7d5fd72b0c6fae0db5871b0aa4ec0fb
Arun Yogi <yogiarun290@gmail.com>
2026-09-25 11:08:43 +0545

    feat : working on jquery button

---
+++ jquerybutton/script.js
@@ -0,0 +1,18 @@
+$(".all").click(function () {
+    $(".img").show();
+});
+
+$(".paper").click(function () {
+    $(".img").hide();
+    $(".img.paper").show();
+});
+
+$(".magazine").click(function () {
+    $(".img").hide();
+    $(".img.magazine").show();
+});
+
+$(".logo").click(function () {
+    $(".img").hide();
+    $(".img.logo").show();
+});
