ecc093b448a2d0b44a3d7bc375900537fc6a6910
Arun Yogi <yogiarun290@gmail.com>
2026-09-25 11:03:38 +0545

    feat : practice owl carousel

---
+++ plugins owl/assign.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 20,
+        nav: false,
+        dots: true,
+        autoplay: false,
+        smartSpeed: 500
+    });
+
+});
